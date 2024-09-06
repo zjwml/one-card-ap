94d906325d0b853690ca667a69b6d04979b63395
zengjun1.fj <zengjun1.fj@ccb.com>
2024-09-06 18:09:44 +0800

    更新数据库语句

--- one-card-controller/src/main/resources/script/init/user_info.sql
+++ one-card-controller/src/main/resources/script/init/user_info.sql
@@ -2,9 +2,12 @@
 
 CREATE TABLE `user_info`
 (
-    `id`        bigint(20)   NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
     `user_name` varchar(100) NOT NULL COMMENT '昵称',
-    `user_id`   varchar(100) DEFAULT NULL COMMENT '用户名',
-    `status`    varchar(2)   DEFAULT '0' COMMENT '0-正常，1-其他',
+    `user_id` varchar(100) DEFAULT NULL COMMENT '用户名',
+    `status` varchar(2) DEFAULT '0' COMMENT '0-正常，1-其他',
+    `room_number` varchar(10) DEFAULT NULL COMMENT '所在房间',
+    `hand` varchar(100) DEFAULT NULL COMMENT '辅助字段，手牌',
+    `ppp` varchar(100) DEFAULT '123456',
     PRIMARY KEY (`id`)
 ) COMMENT ='用户信息表';
